03f40b5f098aa06be932201451dc68f71ed264d8
James Liu <james2006roger@yahoo.ca>
2020-06-16 22:02:03 -0700

    WIP: Refactored configuration initialization/loading, checkpoint loading/saving

--- develop/experiment/experiment.py
+++ develop/experiment/experiment.py
@@ -1,3 +1,6 @@
+import quantization.quantization as custom_quant
+import pruning.pruning as custom_prune
+
 import torch
 import torch.multiprocessing as mp
 import torch.nn.functional as F
@@ -6,10 +9,53 @@ import torch.utils.data.distributed
 from torchvision import datasets, transforms, models
 import horovod.torch as hvd
 import yaml
+from easydict import EasyDict as edict
 from abc import ABC, abstractmethod
 
 import os
 import math
+
+def generate_base_config():
+  config = edict()
+  config.batchSizePerWorker = 32
+  config.numOfThreadsPerWorker = 28
+  config.trainEpochs = 20
+
+  # Learning rate configuration
+  # Constant multiplicative LR with optional warmup
+  config.lrInitialBase = 1e-7
+  config.lrPeakBase = 1e-5
+  config.lrWarmupEpochs = 4
+  config.lrStepSize = 4
+  config.lrReductionFactor = 0.7
+  config.lrMomentum = 0.01
+
+  # Loss contribution
+  config.lossActivationLambda = 0.0
+  config.lossWeightL1Lambda = 0.0
+  config.lossWeightL2Lambda = 0.0
+
+  # Pruning paramters
+  config.prune = False
+  config.pruneCluster = 1
+  config.pruneThreshold = 1e-5
+  config.quantize = False
+
+  # See
+  config.seed = 47
+
+  # Dataset location
+  config.dataTrainDir = './'
+  config.dataValDir = './'
+
+  # Checkpoints
+  config.loadCheckpoint = ''
+  config.loadPretrained = ''
+  config.checkpointLoadPath = ''
+  config.checkpointSaveDir = ''
+  config.checkpointSaveFileNameFormat = ''
+
+  return config
 
 #Helper function for parsing configuration file keys
 def extractConfigKey(config, key):
@@ -32,65 +78,138 @@ class experimentBase():
   """
 
   @abstractmethod
-  def __init__(self, configFile):
-    """
-    Instantiates an experiment object. This is an abstract method with implementation
-    Children initialization methods should call this,
-    and define how 1) the data loader, 2) the model, and 3) the experiment type name
-    :param configFile: Path to YAML file that contains the configuration
-    """
-    #Experiment states initialization
+  def __init__(self, configFile, multiprocessing=False):
+    ##TODO: Children should provide their own init method
+    ##1) Load configuration file
+    ##2) Instantiate and initialize model
+    ##3) Instantiate optimizer
+    ##4) If load from checkpoint is required, then load from checkpoint
+    ##5) Prune network if necessary
+    # Experiment states initialization
     self.bestAccuracy = 0.0
     self.flagFusedQuantized = False
     self.totalEpochElapsed = 0
+    self.multiprocessing = multiprocessing
 
-    #Load experiment setting from config file
-    with open(configFile, 'r') as config:
-      self.configPatchSizePerWorker = extractConfigKey(config, 'batch_size')
-      self.configNumOfWorkers = extractConfigKey(config, 'num_workers')
-      self.configNumOfThreadsPerWorker = extractConfigKey(config, 'threads_per_worker')
-      self.configTrainEpochs = extractConfigKey(config, "num_epoch")
+    qatRoundedConfig = torch.quantization.FakeQuantize.with_args(
+      observer=custom_quant.RoundedMovingAverageMinMaxObserver,
+      quant_min=-128,
+      quant_max=127,
+      averaging_constant=0.01
+    )
 
-      #Learning rate configuration
-      #Constant multiplicative LR with optional warmup
-      self.configInitialBaseLR = extractConfigKey(config, 'initial_base_lr')
-      self.configPeakBaseLR = extractConfigKey(config, 'peak_base_lr')
-      self.configLRWarmUp = extractConfigKey(config, 'warm_up_lr')
-      self.configLRSchedule = extractConfigKey(config, 'schedule_lr')
-      self.configMomentum = extractConfigKey(config, "momentum")
+    self.qatConfig = torch.quantization.QConfig(
+      activation=qatRoundedConfig
+      , weight=qatRoundedConfig
+    )
+    self.optimizer = None
+    self.model = None
 
-      #Loss contribution
-      self.lossActivationLambda = extractConfigKey(config, 'loss_activation_lambda')
-      self.lossWeightL1Lambda = extractConfigKey(config, 'loss_weight_l1_lambda')
-      self.lossWeightL2Lambda = extractConfigKey(config, 'loss_weight_l2_lambda')
+    # Load experiment setting from config file
+    config = generate_base_config()
+    if (multiprocessing is False) or (multiprocessing is True and hvd.rank() == 0):
+      try:
+        file = open(configFile, "r")
+      except IOError:
+        raise ValueError("The provided configuration file cannot be opened.")
+      with file:
+        yamlConfig = yaml.load(file, Loader=yaml.FullLoader)
+        config = edict(yamlConfig)
 
-      #Pruning paramters
-      self.configPrune = extractConfigKey(config, 'prune')
-      self.configPruneCluster = extractConfigKey(config, 'prune_cluster')
-      self.configPruneThreshold = extractConfigKey(config, 'prune_threshold')
-      self.configQuantize = extractConfigKey(config, 'quantize')
+    # Broadcast the configuration to the workers during multiprocessing
+    if multiprocessing is True:
+      config = hvd.broadcast_object(obj=config, root_rank=0, name='config')
 
-      #See
-      self.seed = extractConfigKey(config, 'seed')
+    self.config = config
+    torch.manual_seed(self.config.seed)
+    #End of loading configuration file
 
-      #Path configuration
-      #TODO: Add paths for logging, dataset, loading/saving checkpoint,
-      # and flag for checking whether loading from checkpoint is needed
+    # TODO in the implementation of __init__ methods in the concrete classes
+    # Instantiate data loader
+    # Instantiate model
+    # Quantize the model if quantization is required
 
-
-  def lr_scheduler(self, epoch):
+  def adjust_learning_rate(self, epoch):
     """
-    Compute the learning rate for the current epoch
+    Compute the learning rate for the current epoch, and
+    adjust the learning rate in the optimizer of the experiment
     :return: The learning rate for the current epoch
     """
-    return 1.0
+    MININUM_LR = 1e-5
+    lr = 1e-5
+    if epoch < self.config.lrWarmupEpochs:
+      lr = (epoch / self.config.lrWarmupEpochs * (self.config.lrPeakBase - self.config.lrInitialBase)) \
+           + self.config.lrInitialBase
+    else:
+      lr = self.config.lrReductionFactor ** ((epoch - self.config.lrWarmupEpochs) // self.config.lrStepSize)
+      lr = max(lr, MININUM_LR)
+
+    for param_group in self.optimizer.param_groups:
+      param_group['lr'] = lr
+
+    return lr
+
+  def quantize_model(self):
+    """
+    Helper function, quantize the model
+    :return: None
+    """
+    self.model.fuse_model()
+    self.model.qconfig = self.qatConfig
+    torch.quantization.prepare_qat(self.model, inplace=True)
+    self.flagFusedQuantized = True
+
+
   def restore_experiment_from_checkpoint(self, checkpoint):
     """
     Restores an experiment stats, optimizer, and model from checkpoint
+    When restoring a model, look for the status flag 'quantized'
+    If the model has been quantized, but the experiment doesn't require quantization,
+    then error out.
+    :param checkpoint: Checkpoint path
     :return: None
     """
-    #TODO: Implement this later
-    pass
+    assert self.config.loadPretrained is False, \
+      "Configuration error: cannot resume an experiment on top of a pre-trained model"
+
+    if self.multiprocessing is True:
+      if hvd.rank() == 0:
+        state_dict = torch.load(checkpoint)
+      else:
+        state_dict = None
+      state_dict = hvd.broadcast_object(state_dict, root_rank=0, name='state_dict')
+    else:
+      state_dict = torch.load(checkpoint)
+
+    self.bestAccuracy = state_dict['bestAccuracy']
+    self.totalEpochElapsed = state_dict['totalEpochElapsed']
+    self.flagFusedQuantized = state_dict['fusedQuantized']
+    self.optimizer.load_state_dict(state_dict['optimizer'])
+
+    #Load the model
+    #Check for whether it makes sense to load a quantized experiment
+    #If so, quantize the model before proceed with loading
+    if self.flagFusedQuantized is True:
+      assert self.config.configQuantize is True, \
+        "Loaded experiment contains quantized model, but the experiment config requires"
+
+    if self.config.configQuantize is True:
+      if self.flagFusedQuantized is True:
+        self.quantize_model()
+
+    model_dict = self.model.state_dict()
+
+    #1. Filter out unnecessary keys
+    saved_model_dict = {k: v for k, v in state_dict['model'].items() if k in model_dict}
+    #2. Overwrite entries in the existing model
+    model_dict.update(saved_model_dict)
+    #3. Load the new state dicc
+    self.model.load_state_dict(saved_model_dict)
+
+    if self.config.configQuantize is True:
+      if self.flagFusedQuantized is False:
+        self.quantize_model()
+
 
   def save_experiment_to_checkpoint(self, filepath):
     """
@@ -98,8 +217,18 @@ class experimentBase():
     :param filepath: Path to the checkpoint to be saved
     :return: None
     """
-    #TODO: Implement this later
-    pass
+    if (self.multiprocessing is False) or (self.multiprocessing is True and hvd.rank() == 0):
+      filename = self.config.checkpointSaveFileNameFormat.format(self.totalEpochElapsed)
+      path = os.path.join(self.config.checkpointSaveDir, filename)
+      state = {
+        'bestAccuracy': self.bestAccuracy,
+        'totalEpochElapsed': self.totalEpochElapsed,
+        'fusedQuantized': self.flagFusedQuantized,
+        'optimizer': self.optimizer.state_dict(),
+        'model': self.state_dict()
+      }
+      torch.save(state, path)
+
 
   def train_one_epoch(self):
     """
