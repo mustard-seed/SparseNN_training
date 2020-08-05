64d40fc61aeedbab0d5557a21b202387b7632e3b
James Liu <james2006roger@yahoo.ca>
2020-08-04 21:11:38 -0400

    WIP: tracer hook function

--- develop/tracer/tracer.py
+++ develop/tracer/tracer.py
@@ -1,6 +1,12 @@
 """
 Package for tracing quantized-pruned models in their PyTorch execution order
 """
+from torch import Tensor as T
+from torch import nn as nn
+
+import custom_modules.custom_modules as cm
+import quantization.quantization as qt
+
 from typing import List
 from easydict import EasyDict as edict
 
@@ -174,10 +180,26 @@ class EltAddInfo(LayerInfo):
         self.channels = inputChannels
 
 class TraceDNN:
-    def __init__(self):
-        pass
+    ID_IDX = 0
+    PRECISION_IDX = 0
+    def __init__(self, module: nn.Module):
+        self.module = module
 
-    def insertLayer(layer: LayerInfo) -> None:
+        # Run-time arguments
+        # Running-counter of the layer IDs
+        self.layerID = 0
+        self.layerCount = 0
+        # List of handles for forward-hooks. Used to delete the forward hooks after the first run.
+        self.hookHandles = []
+        # List of layers
+        self.layerList = []
+        # Input adjacency list of registered layer
+        self.inputAdjacencies = []
+        # Output adjacency list of registered layers
+        self.outputAdjacencies = []
+
+
+    def insertLayer(self, layer: LayerInfo) -> None:
         """
         Insert a new layer into the graph
         :param layer: An LayerInfo object
@@ -185,7 +207,7 @@ class TraceDNN:
         """
         pass
 
-    def addForwardEdge(sourceLayerId: int, sinkLayerId: int) -> None:
+    def addForwardEdge(self, sourceLayerId: int, sinkLayerId: int) -> None:
         """
         Add a forward dependency
         :param sourceLayerId: Id of the layer that generates the tensor during inference.
@@ -194,7 +216,7 @@ class TraceDNN:
         """
         pass
 
-    def addBackward(sourceLayerId: int, sinkLayerId: int) -> None:
+    def addBackward(self, sourceLayerId: int, sinkLayerId: int) -> None:
         """
         Add a backward endge
         :param sourceLayerId: Id of the layer that generates the tensor during inference. It will be at the end of the edge
@@ -203,14 +225,14 @@ class TraceDNN:
         """
         pass
 
-    def resetAnnotation() -> None:
+    def resetAnnotation(self) -> None:
         """
         Clears all memory and sparsification flag of layers in the graph
         :return: Hone
         """
         pass
 
-    def annotate(numMemBanks: int) -> None:
+    def annotate(self, numMemBanks: int) -> None:
         """
         Annotates memory and sparsification fields of layers in the graph
         :param numMemBanks:
@@ -218,7 +240,7 @@ class TraceDNN:
         """
         pass
 
-    def dump(fileStream = None) -> str:
+    def dump(self, fileStream = None) -> str:
         """
         Dumps the DNN IR in the inserted execution order as a YAML string.
         If a file stream is provided, the string will be written to the file.
@@ -226,3 +248,70 @@ class TraceDNN:
         :return: The YAML string
         """
         pass
+
+    def traceHook(self, module, input: T, output: T) -> None:
+        """
+        Pytorch NN module forward hook. Used to intercept NN layer execution order and dependency. This function will be
+            called by PyTorch during inference.
+        :param module: The PyTorch NN module to be hooked.
+        :param input: Input tensor. Can be a tuple if the module has multiple inputs.
+        :param output:
+        :return:
+        """
+        # Modify the output
+        # output[0]: layer id of the layer
+        # output[1]: INT8 activation precision
+
+        # Extract the information: input precision(s), input id
+        inputIDs = []
+        inputPrecisions = []
+        if isinstance(input, T):
+            nElements = input.numel()
+            inputIDs.append(input.view(nElements)[self.ID_IDX])
+            inputPrecisions.append(input.view(nElements)[self.PRECISION_IDX])
+        elif isinstance(input, tuple):
+            for tensor in input:
+                nElements = tensor.numel()
+                inputIDs.append(tensor.view(nElements)[self.ID_IDX])
+                inputPrecisions.append(tensor.view(nElements)[self.PRECISION_IDX])
+        else:
+            raise TypeError('The input argument is neither a tensor nor a tuple of tensors')
+
+        # TODO
+        # Instantiate and insert a layer, register input/output adjacencies
+        # If this is a convolution-based layer. Even the fused layer types are Conv2d thanks to inheritance
+        if isinstance(module, (nn.Conv2d, nn.Linear)):
+            pass
+        # if this is an average pooling layer
+        elif isinstance(module, nn.AvgPool2d):
+            pass
+        elif isinstance(module, nn.MaxPool2d):
+            pass
+        elif isinstance(module, cm.EltwiseAdd):
+            pass
+        else:
+            raise TypeError('The tracer hook can only be applied to Conv2d types, Linear types, AvgPool2d types, '
+                            'AvgPool2d, and EltwiseAdd types. Input module type is {}'.format(type(module)))
+
+        # Propagate the layer id, and output precision to the next layer
+        precisionScale = inputPrecisions[0]
+        if hasattr(module, 'activation_post_process'):
+            precisionScale = module.activation_post_process.scale.view(1)[0]
+        else:
+            if isinstance(module, cm.EltwiseAdd):
+                # Number of fraction bits = log2(1/scale)
+                # Number of interger bits = 8 - number of fraction bits
+                # Number of fraction bits new = log2(1/scale_new) = number of fraction bits - 1 = log2(1/scale) - 1
+                # log2(scale) + 1 = log2(scale_new)
+                # scale_new = 2 * scale
+                precisionScale *= 2.0
+            else:
+                pass
+        outputNumel = output.numel()
+        output.view(outputNumel)[self.ID_IDX] = self.layerID
+        output.view(outputNumel)[self.PRECISION_IDX] = precisionScale
+
+        self.layerID += 1
+        self.layerCount += 1
+
+
