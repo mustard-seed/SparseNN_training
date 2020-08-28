2178c05d22f811e6608f42d3aef599c84483aa8d
James Liu <james2006roger@yahoo.ca>
2020-08-28 11:01:43 -0400

    Added a tiny test in the tracer, and fixed the input sizes.

--- develop/testTracer.py
+++ develop/testTracer.py
@@ -16,6 +16,7 @@ from typing import Union, List
 
 import os
 import yaml
+import argparse
 
 def toy_resnet() -> resnet.ResNet:
     """
@@ -29,9 +30,45 @@ def toy_resnet() -> resnet.ResNet:
     network = resnet.ResNet(block, num_blocks, 'cifar10')
     return network
 
-class TracerTest():
+class TinyNet(nn.Module):
+    """
+    Tiny-Net just for generating a trace purpose
+    """
     def __init__(self):
-        self.model = toy_resnet()
+        super().__init__()
+        self.block = resnet.BasicBlock(in_planes=4, planes=8, stride=1)
+        self.averagePool = nn.AvgPool2d(kernel_size=8, divisor_override=64)
+        self.quant = QuantStub()
+        self.dequant = DeQuantStub()
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out")
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+
+    def forward(self, x):
+        x = self.quant(x)
+        x = self.block(x)
+        x = self.averagePool(x)
+        output = self.dequant(x)
+
+        return output
+
+        # Fuse layers prior to quantization
+
+    def fuse_model(self):
+        for m in self.modules():
+            if type(m) == cm.ConvBNReLU:
+                # Fuse the layers in ConvBNReLU module, which is derived from nn.Sequential
+                # Use the default fuser function
+                torch.quantization.fuse_modules(m, ['0', '1', '2'], inplace=True)
+            elif type(m) == cm.ConvBN:
+                torch.quantization.fuse_modules(m, ['0', '1'], inplace=True)
+
+class TracerTest():
+    def __init__(self, useTiny: bool = False):
+        self.useTiny = useTiny
+        self.model = toy_resnet() if useTiny is False else TinyNet()
         self.pruneClusterSize=1
         self.pruneThreshold=1e-4
         qatRoundedConfig = torch.quantization.FakeQuantize.with_args(
@@ -50,12 +87,13 @@ class TracerTest():
         torch.quantization.prepare_qat(self.model, inplace=True)
 
         # Prune the model
-        custom_pruning.applyClusterPruning(
-            self.model.inputConvBNReLU[0],
-            'weight',
-            clusterSize=self.pruneClusterSize,
-            threshold=self.pruneThreshold
-        )
+        if self.useTiny is False:
+            custom_pruning.applyClusterPruning(
+                self.model.inputConvBNReLU[0],
+                'weight',
+                clusterSize=self.pruneClusterSize,
+                threshold=self.pruneThreshold
+            )
         for m in self.model.modules():
             if isinstance(m, resnet.BasicBlock):
                 custom_pruning.applyClusterPruning(m.convBN1[0],
@@ -73,7 +111,7 @@ class TracerTest():
                                                        threshold=self.pruneThreshold)
     def trace(self, dirname: str, fileBaseName: str):
         tracer = Tracer(self.model)
-        dummyInput = torch.randn(size=[16, 3, 32, 32])
+        dummyInput = torch.randn(size=[1, 3, 32, 32]) if self.useTiny is False else torch.randn(size=[1, 4, 8, 8])
         dummyOutput = tracer.traceModel(dummyInput)
         tracer.annotate(numMemRegions=3)
         tracer.dump(dirname, fileBaseName)
@@ -96,7 +134,13 @@ class TracerTest():
         yaml.dump(blobDict, blobFile, default_flow_style=None)
 
 if __name__=='__main__':
+    parser = argparse.ArgumentParser(description="testTracer")
+    parser.add_argument('--tiny', action='store_true',
+                        help='Flag for using the tiny model. Default: False.')
+    args = parser.parse_args()
+
     torch.manual_seed(0)
-    test = TracerTest()
-    test.trace(dirname='.', fileBaseName='testTrace')
+    fileBaseName = 'tinyTrace' if args.tiny is True else 'testTrace'
+    test = TracerTest(useTiny=args.tiny)
+    test.trace(dirname='.', fileBaseName=fileBaseName)
 
