b5234b958161af6ae745431cb52fc02ec525166c
James Liu <james2006roger@yahoo.ca>
2021-04-01 12:26:20 -0400

    Updated environment setup configs for GCP.

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,14 +1,6 @@
 #!/bin/bash 
 
 set -e 
-
-module load gnu8
-module load openmpi3
 export ENV_PREFIX=$PWD/env
-export HOROVOD_WITH_MPI=1
-export HOROVOD_WITHOUT_GLOO=1
-export HOROVOD_CPU_OPERATIONS=MPI
-export HOROVOD_WITHOUT_TENSORFLOW=1
-export HOROVOD_WITHOUT_MXNET=1
 
 conda env create --prefix $ENV_PREFIX --file conda_environment.yml --force
