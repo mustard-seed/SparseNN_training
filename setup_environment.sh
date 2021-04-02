54803630d1ea15d5452256745409cc4ccd0533fa
James Liu <james2006roger@yahoo.ca>
2021-04-02 18:13:17 -0400

    Restored horovod

--- setup_environment.sh
+++ setup_environment.sh
@@ -2,5 +2,11 @@
 
 set -e 
 export ENV_PREFIX=$PWD/env
+export HOROVOD_WITH_MPI=1
+export HOROVOD_CPU_OPERATIONS=MPI
+export HOROVOD_WITHOUT_TENSORFLOW=1
+export HOROVOD_WITHOUT_MXNET=1
+export HOROVOD_NCCL_HOME=$ENV_PREFIX
+export HOROVOD_GPU_OPERATIONS=NCCL
 
 conda env create --prefix $ENV_PREFIX --file conda_environment.yml --force
